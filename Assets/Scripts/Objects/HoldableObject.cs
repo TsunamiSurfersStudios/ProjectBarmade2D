6d980f6f974a230c9d1e14b0027fdc9f90eb1cf0
Mikita Slabysh <slabysh2015@gmail.com>
2025-08-27 02:01:28 -0400

    Fixed drink pick up and hold, improved main scene

--- Assets/Scripts/Objects/HoldableObject.cs
+++ Assets/Scripts/Objects/HoldableObject.cs
@@ -5,20 +5,25 @@ public class HoldableObject : MonoBehaviour
     protected ItemHolder itemHolder;
     protected GameObject item;
 
-    void Start()
-    {
-        itemHolder = GameObject.FindWithTag("Player").GetComponentInChildren<ItemHolder>();
-    }
-
     protected void Give()
     {
+        FindItemHolder();
         itemHolder.GiveObject(gameObject);
     }
 
     protected void Spawn()
     {
+        FindItemHolder();
         GameObject clone = GameObject.Instantiate(item);
         itemHolder.GiveObject(clone);
         clone.SetActive(true);
+    }
+
+    private void FindItemHolder()
+    {
+        if (itemHolder == null)
+        {
+            itemHolder = GameObject.FindWithTag("Player").GetComponentInChildren<ItemHolder>();
+        }
     }
 }
