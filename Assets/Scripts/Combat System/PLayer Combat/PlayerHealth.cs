d46be778081e8cb7020e6a910c7a1a4e327db548
LunaEclipsa <Kackah123@icloud.com>
2025-08-30 09:11:54 -0400

    i thought it was needed :p

--- Assets/Scripts/Combat System/PLayer Combat/PlayerHealth.cs
+++ Assets/Scripts/Combat System/PLayer Combat/PlayerHealth.cs
@@ -10,7 +10,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
