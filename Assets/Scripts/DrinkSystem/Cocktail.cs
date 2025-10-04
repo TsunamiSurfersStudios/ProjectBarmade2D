2cad1852f2c8ddb86d52f1c3d3cc83a9197b8aac
Mikita Slabysh <slabysh2015@gmail.com>
2025-10-04 13:41:57 -0400

    suggestions

--- Assets/Scripts/DrinkSystem/Cocktail.cs
+++ Assets/Scripts/DrinkSystem/Cocktail.cs
@@ -29,3 +29,11 @@ public class Cocktail : MonoBehaviour, IPointerClickHandler
         }
     }
 }
+
+/// Click on ingredient => Read attatched ingreident => Call add ingredient function 
+/// => Reads ingredient type ENUM => Add to drink
+/// Drink mixing station should have a generic call that says "Subtract ingredient"
+/// ClickableComponent or ClickableObject class (similar to InteractironController)
+/// 
+/// Needs to keep track of selected ingredient + amount
+/// Needs to subtract ingredient from storage when added to drink
