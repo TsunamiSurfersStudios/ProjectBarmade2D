1167327ad5e24a59eb6e9dbf4e2be390603e56fa
danaetroupe <danaetroupe@gmail.com>
2025-11-15 10:16:15 -0800

    Delete readme

--- Assets/Tests/PlayMode/DrinkSystem/RecipeControllerTests.cs
+++ Assets/Tests/PlayMode/DrinkSystem/RecipeControllerTests.cs
@@ -25,7 +25,6 @@ public class RecipeControllerTests
     }
 
     [Test]
-    [LoadScene(PATH)]
     public void VerifyAllIngredientsUsedTest()
     {
         bool allUsed = controller.VerifyAllIngredientsUsed();
