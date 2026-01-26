f67a1a82ec986d6558598df4a59c625d894c485b
skatermage <MEGANCM9@AOL.COM>
2026-01-26 16:43:08 -0500

    deleting d again

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject pauseMenu;
+   
+
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu = GameObject.Find("PauseMenu");
+        pauseMenu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+   
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1f; //TODO: Add this to TimeController
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        //SceneManager.LoadScene("MainMenu"); 
+    }
+
+}
