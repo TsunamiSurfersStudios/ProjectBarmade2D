8ab45c377e432c2701b5d82918d26169aacb96d3
Marina <140115877+mhmarina@users.noreply.github.com>
2026-01-30 19:47:17 -0700

    Added Sprites to test Ingredients

--- Assets/Scripts/DrinkSystem/RecipeController.cs
+++ Assets/Scripts/DrinkSystem/RecipeController.cs
@@ -14,9 +14,15 @@ public class Tier
 }
 public class RecipeController : MonoBehaviour
 {
-    [SerializeField] Tier[] tiers;
+    [SerializeField] Tier[] tiers; // tiers (or levels) can maybe be scriptable objects
+                                   // so we can persist and easily decouple
+                                   // their data from this script. We would just need 
+                                   // to pass the tier to the methods here
+                                   // And this script itself can be static / singleton
+                                   // or even a regular class so we don't need to always
+                                   // have one in the scene
+
     [SerializeField] string PATH = "Bar"; // For testing purposes only
-    //[SerializeField] GameEvent 
 
     public bool VerifyRecipeControllerSetup()
     {
