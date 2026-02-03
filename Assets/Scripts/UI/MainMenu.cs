2f03c04c461c1d5c6c073aede3599b5dc2bfe421
skatermage <MEGANCM9@AOL.COM>
2026-02-03 00:14:01 -0500

    Settings and MainMenu stuff

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("PlayTests"); // TODO: make this an actual loadgame logic 
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene("PlayTests"); // for now loads the scene PlayTests
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -58,7 +58,7 @@ public class PauseMenu : MonoBehaviour
 
     public void MainMenu()
     {
-        //SceneManager.LoadScene("MainMenu"); 
+        SceneManager.LoadScene("MainMenu"); 
     }
 
 }
